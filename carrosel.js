6793950296e44e9c7c03b786aacf1f66bba7cfbf
ESousa97 <sousa3086@outlook.com>
2023-11-15 22:40:45 -0300

    Alteração na pagina principal

--- carrosel.js
+++ carrosel.js
@@ -1,31 +1,38 @@
-const numCards = 8; // Número total de cards
-const slider = document.getElementById('slickSlider');
-const arrowLeft = document.querySelector('.arrow-left');
-const arrowRight = document.querySelector('.arrow-right');
+document.addEventListener('DOMContentLoaded', function () {
+  const numCards = 8; // Número total de cards
+  const slider = document.getElementById('slickSlider');
+  const arrowLeft = document.querySelector('.arrow-left');
+  const arrowRight = document.querySelector('.arrow-right');
 
-// Clonar os cards dinamicamente
-for (let i = 1; i < numCards; i++) {
-  const originalCards = Array.from(slider.children);
-  originalCards.forEach(originalCard => {
-    const clonedCard = originalCard.cloneNode(true);
-    slider.appendChild(clonedCard);
+  // Adicionar eventos de mouse para os cards
+  const cards = document.querySelectorAll('.icon-cards__item');
+  cards.forEach(card => {
+    card.addEventListener('mouseover', () => bringCardToFront(card));
   });
-}
 
-let currentIndex = numCards - 1; // Iniciar com o último card para centralizar o card 1
+  function bringCardToFront(selectedCard) {
+    cards.forEach(card => {
+      card.style.zIndex = 1; // Coloca todos os cards no fundo
+    });
+    selectedCard.style.zIndex = 2; // Coloca o card selecionado na frente
+  }
 
-function handleMouseOver(card) {
-  const index = Array.from(card.parentNode.children).indexOf(card);
-  const offset = index - currentIndex;
-  rotateSlider(offset);
-}
+  // Adicionando eventos de clique para os botões de navegação
+  arrowLeft.addEventListener('click', () => rotateSlider(-1));
+  arrowRight.addEventListener('click', () => rotateSlider(1));
+
+  // Inicializar o carrossel centralizando o card 1
+  updateSliderPosition();
+});
+
+let currentIndex = 0;
 
 function rotateSlider(offset) {
   currentIndex += offset;
-  const maxIndex = slider.childElementCount / numCards; // Total de cards originais
+  const maxIndex = Math.ceil(currentIndex / numCards); // Arredonda para cima o total de grupos de cards
   if (currentIndex < 0) {
-    currentIndex = maxIndex - 1;
-  } else if (currentIndex >= maxIndex) {
+    currentIndex = maxIndex * numCards - 1;
+  } else if (currentIndex >= maxIndex * numCards) {
     currentIndex = 0;
   }
   updateSliderPosition();
@@ -33,27 +40,8 @@ function rotateSlider(offset) {
 
 function updateSliderPosition() {
   const cardWidth = slider.firstElementChild.offsetWidth;
-  const translateValue = -currentIndex * cardWidth;
+  const containerWidth = slider.offsetWidth;
+  const translateValue = -(currentIndex % numCards) * (cardWidth / 2) + (containerWidth / 2 - cardWidth / 2);
+  slider.style.transition = 'transform 0.6s ease-out'; // Adiciona uma transição suave
   slider.style.transform = `translateX(${translateValue}px)`;
 }
-
-function navigateSlider(direction) {
-  rotateSlider(direction);
-}
-
-// Adicionando lógica de rotação automática
-function rotateSliderAutomatically() {
-  rotateSlider(1); // Rotacionar para o próximo card
-}
-
-// Adicionando eventos de clique para os botões de navegação
-arrowLeft.addEventListener('click', () => navigateSlider(-1));
-arrowRight.addEventListener('click', () => navigateSlider(1));
-
-if (window.innerWidth <= 768) {
-  arrowLeft.style.cursor = 'default';
-  arrowRight.style.cursor = 'default';
-}
-
-// Centralizando o card 1
-updateSliderPosition();
